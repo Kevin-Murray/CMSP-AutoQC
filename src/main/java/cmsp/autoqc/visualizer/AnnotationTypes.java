26c1d0e639c3574cc1e7b0e9a733bd20d2d06a09
Kevin-Murray <kjmurray1592@gmail.com>
2023-02-10 10:46:17 -0600

    Add AnnotationTypes

---
+++ src/main/java/cmsp/autoqc/visualizer/AnnotationTypes.java
@@ -0,0 +1,33 @@
+package cmsp.autoqc.visualizer;
+
+public enum AnnotationTypes {
+
+    STANDARD("New Stock Solution"),
+    COLUMN("Column Change"),
+    MAINTENANCE("Instrument Maintenance"),
+    MOBILE("Mobile Phase Change"),
+    TUNE("New Tune / Calibration"),
+    OTHER("Other");
+
+    private final String label;
+
+    AnnotationTypes(String s) {
+        this.label = s;
+    }
+
+    public static String getAnnotationType(String type) {
+        for(AnnotationTypes i : values()){
+            if(i.label.equals(type)){
+                return i.name();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return this.label;
+    }
+
+}

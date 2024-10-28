1a2e2ed5153b82d3cb5d826da2088c02d2c3a065
Kevin-Murray <kjmurray1592@gmail.com>
2024-10-28 11:51:12 -0500

    Updated annotation point colors.

--- src/main/java/cmsp/quickqc/visualizer/utils/annotations/AnnotationStyles.java
+++ src/main/java/cmsp/quickqc/visualizer/utils/annotations/AnnotationStyles.java
@@ -2,30 +2,36 @@ package cmsp.quickqc.visualizer.utils.annotations;
 
 public enum AnnotationStyles {
 
-    STANDARD("-fx-background-color: #1B9E77, white;\n"
-            + " -fx-background-insets: 0, 4;\n"
-            + " -fx-background-radius: 10px;\n"
-            + " -fx-padding: 10px;"),
-    COLUMN("-fx-background-color: #D95F02, white;\n"
-            + " -fx-background-insets: 0, 4;\n"
-            + " -fx-background-radius: 10px;\n"
-            + " -fx-padding: 10px;"),
-    MAINTENANCE("-fx-background-color: #7570B3, white;\n"
-            + " -fx-background-insets: 0, 4;\n"
-            + " -fx-background-radius: 10px;\n"
-            + " -fx-padding: 10px;"),
-    MOBILE("-fx-background-color: #E7298A, white;\n"
-            + " -fx-background-insets: 0, 4;\n"
-            + " -fx-background-radius: 10px;\n"
-            + " -fx-padding: 10px;"),
-    TUNE("-fx-background-color: #A6761D, white;\n"
-            + " -fx-background-insets: 0, 4;\n"
-            + " -fx-background-radius: 10px;\n"
-            + " -fx-padding: 10px;"),
-    OTHER("-fx-background-color: #E6AB02, white;\n"
-            + " -fx-background-insets: 0, 4;\n"
-            + " -fx-background-radius: 10px;\n"
-            + " -fx-padding: 10px;");
+    STANDARD("""
+            -fx-background-color: #1B9E77, white;
+             -fx-background-insets: 0, 4;
+             -fx-background-radius: 10px;
+             -fx-padding: 10px;"""),
+    COLUMN("""
+            -fx-background-color: #D95F02, white;
+             -fx-background-insets: 0, 4;
+             -fx-background-radius: 10px;
+             -fx-padding: 10px;"""),
+    MAINTENANCE("""
+            -fx-background-color: #C8A2C8, white;
+             -fx-background-insets: 0, 4;
+             -fx-background-radius: 10px;
+             -fx-padding: 10px;"""),
+    MOBILE("""
+            -fx-background-color: #A6CAEC, white;
+             -fx-background-insets: 0, 4;
+             -fx-background-radius: 10px;
+             -fx-padding: 10px;"""),
+    TUNE("""
+            -fx-background-color: #E7298A, white;
+             -fx-background-insets: 0, 4;
+             -fx-background-radius: 10px;
+             -fx-padding: 10px;"""),
+    OTHER("""
+            -fx-background-color: #E6AB02, white;
+             -fx-background-insets: 0, 4;
+             -fx-background-radius: 10px;
+             -fx-padding: 10px;""");
 
     private final String label;
 
@@ -33,7 +39,7 @@ public enum AnnotationStyles {
         this.label = s;
     }
 
-    public static String getAnnotationStyle(String type) {
+    public static String getAnnotationName(String type) {
         for(AnnotationStyles i : values()){
             if(i.label.equals(type)){
                 return i.name();

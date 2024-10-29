6f6f8469d5e584d5e761ce56694ebdfcc0bc6882
Kevin-Murray <kjmurray1592@gmail.com>
2024-10-29 10:01:55 -0500

    Added application icon.

--- src/main/java/cmsp/quickqc/visualizer/QuickQCMain.java
+++ src/main/java/cmsp/quickqc/visualizer/QuickQCMain.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,13 +15,16 @@ public class QuickQCMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        stage.setTitle("CMSP - QuickQC Visualizer (v. 1.1.0)");
+        stage.setTitle("CMSP - QuickQC Visualizer (v. 1.3.0)");
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainPageGui.fxml"));
         Parent mainPage = fxmlLoader.load();
 
         Scene main = new Scene(mainPage);
         main.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styleGuide.css")).toString());
+
+        Image image = new Image("cmsp/quickqc/visualizer/quickQA-icon.png");
+        stage.getIcons().add(image);
 
         stage.setResizable(false);
         stage.setScene(main);

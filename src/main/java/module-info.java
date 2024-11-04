41ddb928347741598f2c1212495ab72d0a6fb337
Kevin-Murray <kjmurray1592@gmail.com>
2024-11-04 16:07:01 -0600

    Updated module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,16 @@
 module cmsp.quickqc.visualizer {
+
     requires javafx.controls;
     requires javafx.fxml;
     requires java.prefs;
 
     opens cmsp.quickqc.visualizer.gui to javafx.fxml;
     opens cmsp.quickqc.visualizer.datamodel to javafx.fxml;
+    opens cmsp.quickqc.visualizer.utils.plotUtils to javafx.fxml;
 
     exports cmsp.quickqc.visualizer;
     exports cmsp.quickqc.visualizer.parameters;
     exports cmsp.quickqc.visualizer.datamodel;
     exports cmsp.quickqc.visualizer.utils.plotUtils;
-    opens cmsp.quickqc.visualizer.utils.plotUtils to javafx.fxml;
+    exports cmsp.quickqc.visualizer.utils.annotations;
 }

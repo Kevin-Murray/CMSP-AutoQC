9a3e91365091c75b59d4535652601c1e4f87c25e
Kevin-Murray <kjmurray1592@gmail.com>
2024-11-18 10:00:13 -0600

    Handling of log numbers, annotations and guide set parameters.

--- src/main/java/cmsp/quickqc/visualizer/datamodel/Parameters.java
+++ src/main/java/cmsp/quickqc/visualizer/datamodel/Parameters.java
@@ -1,11 +1,14 @@
 
-package cmsp.quickqc.visualizer.parameters;
+package cmsp.quickqc.visualizer.datamodel;
 
+import cmsp.quickqc.visualizer.enums.DatabaseTypes;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,52 +19,36 @@ public class Parameters {
     public String instrument;
     public String configuration;
     public String matrix;
+    public String standard;
     public String report;
-    public String plotScale;
+    public String selection;
     public String dateRange;
-
-    public int plotType;
-
+    public String varType; // Variability guides
+    public int plotType; // Type of Line chart, e.g. - LeveyJennings
     public LocalDate startDate;
     public LocalDate endDate;
-
-    public Map<String, Boolean> annotationMap;
-
-    public String varType;
     public Boolean logScale;
     public Boolean showExcluded;
-    private Boolean showGuide;
-
+    public Boolean showGuide;
     public Path databasePath;
+    public Path reportPath;
+    public List<String> logNumbers;
+    public Map<String, Boolean> annotationMap;
 
     /**
      * Null-constructor for parameters class.
      */
     public Parameters() {
 
-        instrument = null;
-        configuration = null;
-        matrix = null;
-        report = null;
-        plotScale = null;
-        plotType = 0;
-        startDate = null;
-        endDate = null;
-        varType = null;
-        annotationMap = null;
-        logScale = null;
-        showExcluded = null;
-        showGuide = null;
     }
 
     /**
      * Constructor for parameters class. Formats user input selections or preferences.
      */
-    public Parameters(ChoiceBox<String> instrumentBox,
-                      ChoiceBox<String> configurationBox,
-                      ChoiceBox<String> matrixBox,
+    public Parameters(ReportContext reportContext,
                       ChoiceBox<String> reportBox,
                       ChoiceBox<String> dateRangeBox,
+                      List<String> logNumbers,
                       RadioButton leveyJenningsButton,
                       RadioButton movingRangeButton,
                       RadioButton cusummButton,
@@ -75,11 +62,17 @@ public class Parameters {
                       Boolean showGuideSet,
                       Path databasePath) {
 
-        this.instrument = instrumentBox.getSelectionModel().getSelectedItem();
-        this.configuration = configurationBox.getSelectionModel().getSelectedItem();
-        this.matrix = matrixBox.getSelectionModel().getSelectedItem();
+        // Set user input parameters.
+        this.instrument = reportContext.instrument();
+        this.configuration = reportContext.config();
+        this.matrix = reportContext.matrix();
+        this.standard = reportContext.standard();
         this.report = reportBox.getSelectionModel().getSelectedItem();
         this.dateRange = dateRangeBox.getSelectionModel().getSelectedItem();
+        this.logNumbers = logNumbers;
+
+        // User variable selection, expects column headers to be in form: Standard - Variable
+        this.selection = standard + " - " + report;
 
         // TODO - this needs to be handled better.
         if(leveyJenningsButton.isSelected()) {
@@ -99,17 +92,16 @@ public class Parameters {
             this.plotType = 4;
         }
 
+        // Variability guides and annotation map assignment
         this.varType = varType;
         this.annotationMap = annotationMap;
-
         this.startDate = startDatePicker.getValue();
         this.endDate = endDatePicker.getValue();
-
         this.logScale = logScale;
         this.showExcluded = showExcluded;
-        this.showGuide = false;
-
+        this.showGuide = showGuideSet;
         this.databasePath = databasePath;
+        this.reportPath = reportContext.database();
     }
 
     /**
@@ -122,6 +114,7 @@ public class Parameters {
         return !(this.instrument == null &&
                 this.configuration == null &&
                 this.matrix == null &&
+                this.standard == null &&
                 this.report == null &&
                 this.dateRange == null);
     }
@@ -144,5 +137,27 @@ public class Parameters {
 
             return true;
         }
+    }
+
+    /**
+     * Get database file path based on user selected Instrument-Configuration-Matrix combination.
+     *
+     * @return File path of context-specific database CSV
+     */
+    public Path getPath() {
+
+        //return parameters.databasePath.resolve(reportMap.get(hash));
+        return this.databasePath.resolve(this.reportPath);
+    }
+
+    /**
+     * Get path of annotation database CSV
+     *
+     * @return Annotation database CSV path
+     */
+    public Path getAnnotationPath() {
+
+        // TODO - annotation database path should be handled by enum
+        return this.databasePath.resolve(Paths.get(DatabaseTypes.ANNOTATION.getFileName()));
     }
 }

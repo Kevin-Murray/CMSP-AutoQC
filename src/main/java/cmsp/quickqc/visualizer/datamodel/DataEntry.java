aca6cc0c3607792a70a705c4a2a0a185b87e63ee
Kevin-Murray <kjmurray1592@gmail.com>
2024-11-18 09:56:15 -0600

    Add method comments. Added methods to handle guide status.

--- src/main/java/cmsp/quickqc/visualizer/datamodel/DataEntry.java
+++ src/main/java/cmsp/quickqc/visualizer/datamodel/DataEntry.java
@@ -1,15 +1,27 @@
+
 package cmsp.quickqc.visualizer.datamodel;
 
 import java.util.*;
 
 import static cmsp.quickqc.visualizer.utils.MathUtils.log2;
 
+/**
+ * QC data class. Stores all tabular entries as a LinkedHashMap.
+ * For plotting purposes, Annotations may be stored as DataEntry
+ */
 public class DataEntry {
 
     final private LinkedHashMap<String, String> entries;
     final private String type;
 
-    public DataEntry(String[] header, String[] entry, String type){
+    /**
+     * Constructor method.
+     *
+     * @param header Column headers from report table
+     * @param entry Variable values that user intends to monitor
+     * @param type Indicator whether entry is faux-annotation entry
+     */
+    public DataEntry(String[] header, String[] entry, String type) {
 
         this.entries = new LinkedHashMap<>();
         addItems(header, entry);
@@ -17,102 +29,214 @@ public class DataEntry {
         this.type = type;
     }
 
-    public void addItems(String[] header, String[] items){
+    /**
+     * Put each value into designated bucket.
+     *
+     * @param header Column names from QC report
+     * @param items Variable values
+     */
+    public void addItems(String[] header, String[] items) {
 
-        for(int i = 0; i < items.length; i++){
+        for(int i = 0; i < items.length; i++) {
 
             entries.put(header[i], items[i]);
-
         }
     }
 
-    public void replaceItem(String key, String value){
+    /**
+     * Replace value at key with new value.
+     *
+     * @param key Column name key
+     * @param value New value
+     */
+    public void replaceItem(String key, String value) {
+
         entries.put(key, value);
     }
 
-    public String getDate(){
+    /**
+     * Get date of entry acquisition.
+     *
+     * @return Date as string
+     */
+    public String getDate() {
+
         return entries.get("Date");
     }
 
-    public Boolean excludeData(){
+    /**
+     * Is DataEntry marked as excluded from the QC line chart.
+     *
+     * @return Boolean exclusion status
+     */
+    public Boolean isExcluded() {
+
         return entries.get("Show").equals("Exclude");
     }
 
-    public Double getItem(String key){
+    /**
+     * Get value at key as Double.
+     *
+     * @param key Column header key
+     * @return Double of value string
+     */
+    public Double getItem(String key) {
 
         return Double.parseDouble(entries.get(key));
     }
 
-    public Double getItem(String key, Boolean logScale){
+    /**
+     * Get value at key as Log2 transformed Double.
+     *
+     * @param key Column header key
+     * @param logScale Boolean logScale transformation indicator
+     * @return Double of log2 transformed value string
+     */
+    public Double getItem(String key, Boolean logScale) {
 
-        Double value;
-
-        if(logScale){
-            value = log2(this.getItem(key));
-        } else {
-            value = this.getItem(key);
-        }
-
-        return value;
+        return (logScale) ? log2(this.getItem(key)) : this.getItem(key);
     }
 
-    public String getValue(String key){
+    /**
+     * Get value at key as string
+     *
+     * @param key Column header key
+     * @return String of value
+     */
+    public String getValue(String key) {
+
         return entries.get(key);
     }
 
+    /**
+     * Get column header keys as Set
+     *
+     * @return Set of column header strings
+     */
     public Set<String> getKeySet() {
 
         return entries.keySet();
     }
 
+    /**
+     * Size of this DataEntry LinkedHashSet variable
+     *
+     * @return int this LinkedHashSet size
+     */
     public int size(){
         return this.entries.size();
     }
 
-    public void setExcluded(){
+    /**
+     *  Change DataEntry exclusion status to opposite status
+     */
+    public void changeExclusionStatus() {
 
         String exclusion = this.entries.get("Show");
 
-        if(exclusion.equals("Include")){
+        if(exclusion.equals("Include")) {
+
             this.entries.put("Show", "Exclude");
+
         } else {
+
             this.entries.put("Show", "Include");
         }
-
     }
 
+    /**
+     * Change DataEntry guide set status to opposite status
+     */
+    public void changeGuideStatus() {
+
+        String exclusion = this.entries.get("Guide");
+
+        // This is a little wonky because all entries are stored as strings.
+        if(exclusion.equals("TRUE")) {
+
+            this.entries.put("Guide", "FALSE");
+
+        } else {
+
+            this.entries.put("Guide", "TRUE");
+        }
+    }
+
+    /**
+     * Set DataEntry comment
+     *
+     * @param comment User input comment
+     */
     public void setComment(String comment) {
 
         this.entries.put("Comment", comment.replace("\n", "\\n"));
     }
 
+    /**
+     * Get DataEntry comment
+     *
+     * @return Comment string
+     */
     public String getComment() {
 
         return(this.entries.get("Comment").replace("\\n", "\n"));
     }
 
+    /**
+     * Get DataEntry experimental log number
+     *
+     * @return Log number string
+     */
+    public String getLogNumber() {
+
+        return this.entries.get("Log");
+    }
+
+    /**
+     * Write as DataEntry values as string wrapped with quotation marks for database writing.
+     *
+     * @return List of strings of each value in quotation marks
+     */
     public List<String> writeValues() {
 
         List<String> values = new ArrayList<>();
-
         Set<String> keys = this.getKeySet();
 
         for(String key : keys) {
 
             values.add("\"" + this.getValue(key) + "\"");
-
         }
 
         return(values);
     }
 
+    /**
+     * Get type of DataEntry - QC report value or annotation type.
+     *
+     * @return String of entry type
+     */
     public String getType() {
 
         return this.type;
     }
 
+    /**
+     * Is DataEntry an annotation or QC report value.
+     *
+     * @return Boolean true if annotation
+     */
     public Boolean isAnnotation() {
+
         return !this.type.equals("Sample");
     }
 
+    /**
+     * Is DataEntry part of report guide set.
+     *
+     * @return Boolean true if part of guide set
+     */
+    public Boolean isGuide() {
+
+        return Boolean.valueOf(this.entries.get("Guide"));
+    }
 }
